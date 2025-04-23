ec7372edd8ccd611e5c28aafa8fac16290df495b
Avi Watwani <avi.watwani@gmail.com>
2025-04-23 10:48:19 +0530

    Add tests for two numbers input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -9,10 +9,16 @@ RSpec.describe StringCalculator do
     # Handle single number input
     it "returns the number itself when given a single number" do
       expect(StringCalculator.add("1")).to eq(1)
+      expect(StringCalculator.add("5")).to eq(5)
     end
 
-    it "returns another single number correctly" do
-      expect(StringCalculator.add("5")).to eq(5)
+    # Handle two numbers input
+    it "returns the sum of two numbers separated by a comma" do
+      expect(StringCalculator.add("1,2")).to eq(3)
+    end
+
+    it "returns the sum of two different numbers separated by a comma" do
+      expect(StringCalculator.add("10,20")).to eq(30)
     end
   end
 end
