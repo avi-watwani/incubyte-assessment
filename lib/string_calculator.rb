b14212c7dcee6235aec447e4e8ed3734746d0ffb
Avi Watwani <avi.watwani@gmail.com>
2025-04-23 11:09:23 +0530

    Refactor add method to improve delimiter handling and enhance readability

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,8 +1,27 @@
 class StringCalculator
-  def self.add(numbers_string)
-    return 0 if numbers_string.empty?
+  def self.add(input_string)
+    return 0 if input_string.empty?
 
-    numbers = numbers_string.split(/,|\n/) # regex to split by comma or newline
+    delimiter = /,|\n/ # Default delimiters
+    numbers_part = input_string
+
+    # Check for custom delimiter definition
+    if input_string.start_with?("//")
+      match = input_string.match(/^\/\/(.)\n(.*)/m) # m for multiline matching of \n
+      if match
+        custom_delimiter = match[1]
+        numbers_part = match[2] # The rest of the string containing numbers
+        delimiter = custom_delimiter
+      end
+    end
+
+    # Conditional split
+    numbers = if delimiter.is_a?(Regexp)
+                numbers_part.split(delimiter)
+              else
+                numbers_part.split(delimiter)
+              end
+
     numbers.map(&:to_i).sum
   end
 end
